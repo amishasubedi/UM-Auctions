c3899ff2fca4d8a4cc32c953c40b6da6a016661f
asubedi <asubedi@go.olemiss.edu>
2022-10-20 22:40:19 -0500

    connected to mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 const app = require("./app");
+const connectDatabase = require("./config/database");
 
 const dotenv = require("dotenv");
 
 //set up config file
 dotenv.config({ path: "backend/config/config.env" });
+
+// connect to mongodb, call the function
+connectDatabase();
 
 app.listen(process.env.PORT, () => {
   console.log(
