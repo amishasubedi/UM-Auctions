3a00c62eeff1a26126dd48eecd3235aa88495967
asubedi <asubedi@go.olemiss.edu>
2022-10-26 13:55:51 -0500

    frontend setup

---
+++ frontend/src/pages/Register.js
@@ -0,0 +1,5 @@
+const Register = () => {
+  <h2>Register Page</h2>;
+};
+
+export default Register;
---
+++ frontend/src/pages/user/userCart.js
@@ -0,0 +1,5 @@
+const userCart = () => {
+  return <h2>User Cart</h2>;
+};
+
+export default userCart;
