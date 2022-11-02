c8be62a5ddb08500efa858e28c84c4d3677efec2
asubedi <asubedi@go.olemiss.edu>
2022-11-01 23:04:45 -0500

    update product error

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -6,11 +6,12 @@ const {
   getProducts,
   newProduct,
   getSingleProduct,
+  updateProduct,
 } = require("../controllers/productController");
 
 router.route("/products").get(getProducts);
 router.route("/products/:id").get(getSingleProduct);
-
 router.route("/products/new").post(newProduct);
+router.route("/admin/products/:id").put(getSingleProduct); // to edit
 
 module.exports = router;
