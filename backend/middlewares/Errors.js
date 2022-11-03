040f1b17e945701abc7b60e7b22d8d848668cd9e
asubedi <asubedi@go.olemiss.edu>
2022-11-03 15:21:31 -0500

    handled dev, prod, and async errors

--- backend/middlewares/Errors.js
+++ backend/middlewares/Errors.js
@@ -2,10 +2,26 @@ const ErrorHandler = require("../utils/ErrorHandler");
 
 module.exports = (err, req, res, next) => {
   err.errorCode = err.errorCode || 500;
-  err.message = err.message || "Internal Server Error";
+  // err.message = err.message || "Internal Server Error";
 
-  res.status(err.errorCode).json({
-    success: false,
-    error: err,
-  });
+  // development errors
+  if (process.env.NODE_ENV === "DEVELOPMENT") {
+    res.status(err.errorCode).json({
+      success: false,
+      error: err,
+      errMessage: err.message,
+      stack: err.stack,
+    });
+  }
+
+  // production errors
+  if (process.env.NODE_ENV === "PRODUCTION") {
+    let error = { ...err }; // destructing to copy erros
+    error.message = err.message;
+
+    res.status(err.errorCode).json({
+      success: false,
+      message: error.message || "Internal Server Error",
+    });
+  }
 };
--- backend/models/product.js
+++ backend/models/product.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const productSchema = new mongoose.Schema({
   name: {
     type: String,
-    // required: [true, "Please enter product name"],
+    required: [true, "Please enter product name"],
     trim: true,
     maxLength: [100, "Product name cannot exceed 100 characters"],
   },
