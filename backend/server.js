9697fa2886b960f4b1d865e0657d3c6cc679b65e
asubedi <asubedi@go.olemiss.edu>
2022-11-03 15:31:43 -0500

    promise rejections error

--- backend/server.js
+++ backend/server.js
@@ -9,8 +9,19 @@ dotenv.config({ path: "backend/config/config.env" });
 // connect to mongodb, call the function
 connectDatabase();
 
-app.listen(process.env.PORT, () => {
+const server = app.listen(process.env.PORT, () => {
   console.log(
     `SERVER started on port: ${process.env.PORT} in ${process.env.NODE_ENV} mode.`
   );
+});
+
+const serverCloseHandler = () => {
+  process.exit(1);
+};
+
+// promise rejections errors
+process.on("unhandledRejection", (err) => {
+  console.log(`ERROR: ${err.message}`);
+  console.log("Server down due to unhandled promise rejection");
+  server.close(serverCloseHandler());
 });
