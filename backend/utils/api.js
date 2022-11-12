1b4944cf73894af7a7a87e286dd9f489069ff4e1
asubedi <asubedi@go.olemiss.edu>
2022-11-12 12:40:14 -0600

    filter by price and category

--- backend/utils/api.js
+++ backend/utils/api.js
@@ -19,6 +19,25 @@ class APIProduct {
     this.query = this.query.find({ ...keyword });
     return this;
   }
+
+  filter() {
+    const dummy = { ...this.queryString };
+    //console.log(dummy)
+
+    // filter by category
+    const filterCategory = ["keyword", "limit", "page"];
+    filterCategory.forEach((i) => delete dummy[i]);
+
+    // filter by price
+    let queryString = JSON.stringify(dummy);
+    queryString = queryString.replace(
+      /\b(gt|gte|lt|lte)\b/g, // mongo operators to convert string into dollars
+      (match) => `$${match}`
+    );
+
+    this.query = this.query.find(JSON.parse(queryString));
+    return this;
+  }
 }
 
 module.exports = APIProduct;
