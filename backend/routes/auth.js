75a7982a276463485f84891d0f0be6b0f5e160ed
asubedi <asubedi@go.olemiss.edu>
2022-11-16 19:45:51 -0600

    list of logged in users

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -7,11 +7,15 @@ const {
   logout,
   forgotPassword,
   resetPassword,
+  getUserProfile,
 } = require("../controllers/userController");
+
+const { isAuthenticated } = require("../middlewares/auth");
 
 router.route("/register").post(registerUser);
 router.route("/login").post(authenticate);
 router.route("/forgot/password").post(forgotPassword);
+router.route("/user").get(isAuthenticated, getUserProfile);
 router.route("/password/reset/:token").put(resetPassword);
 router.route("/logout").get(logout);
 
