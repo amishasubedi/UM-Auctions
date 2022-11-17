32028c5bc5f6db2b685f786f3a707ef76842b70d
asubedi <asubedi@go.olemiss.edu>
2022-11-16 20:42:56 -0600

    worked with admin priviledge to see all users

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -181,3 +181,26 @@ exports.editProfile = AsyncErrors(async (req, res, next) => {
     success: true,
   });
 });
+
+// ADMIN ROUTES
+exports.allUsers = AsyncErrors(async (req, res, next) => {
+  const users = await User.find();
+
+  res.status(200).json({
+    success: true,
+    users,
+  });
+});
+
+exports.userDetails = AsyncErrors(async (req, res, next) => {
+  const user = await User.findById(req.params.id);
+
+  if (!user) {
+    return next(new ErrorHandler("User not found"));
+  }
+
+  res.status(200).json({
+    success: true,
+    user,
+  });
+});
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -10,9 +10,11 @@ const {
   getUserProfile,
   updatePassword,
   editProfile,
+  allUsers,
+  userDetails,
 } = require("../controllers/userController");
 
-const { isAuthenticated } = require("../middlewares/auth");
+const { isAuthenticated, assignRole } = require("../middlewares/auth");
 
 router.route("/register").post(registerUser);
 router.route("/login").post(authenticate);
@@ -24,5 +26,14 @@ router.route("/edit/profile").put(isAuthenticated, editProfile);
 router.route("/user").get(isAuthenticated, getUserProfile);
 router.route("/password/reset/:token").put(resetPassword);
 router.route("/logout").get(logout);
+
+// Admin
+router
+  .route("/admin/users")
+  .get(isAuthenticated, assignRole("admin"), allUsers);
+
+router
+  .route("/admin/user/:id")
+  .get(isAuthenticated, assignRole("admin"), userDetails);
 
 module.exports = router;
