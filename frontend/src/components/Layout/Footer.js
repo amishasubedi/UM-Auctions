ed7ae008354ee3c5e168dd3fb2dbb7897c8d0c2a
asubedi <asubedi@go.olemiss.edu>
2022-11-17 19:27:23 -0600

    implemented homepage ui

---
+++ frontend/src/components/Layout/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Fragment } from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <Fragment>
+      <footer className="py-1">
+        <p className="text-center mt-1">
+          UM Auctions - 2021-2022, All Rights Reserved
+        </p>
+      </footer>
+    </Fragment>
+  );
+};
+
+export default Footer;
