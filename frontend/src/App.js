05674531dbd576850dac91e5b2bbc6c0a7b0f3b4
asubedi <asubedi@go.olemiss.edu>
2022-11-17 19:27:23 -0600

    implemented homepage ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,17 @@
+import { Fragment } from "react";
 import "./App.css";
+import Home from "./components/Home/Home";
+import Footer from "./components/Layout/Footer";
+import Header from "./components/Layout/Header";
 
 function App() {
-  return <h2>Auction</h2>;
+  return (
+    <Fragment>
+      <Header />
+      <Home />
+      <Footer />
+    </Fragment>
+  );
 }
 
 export default App;
