895e21569146cd7b097260fa4bab4dd7efa47a37
asubedi <asubedi@go.olemiss.edu>
2022-11-19 21:47:30 -0600

    fixed fetch image bug, worked with product details ui

--- frontend/src/reducers/product_constants.js
+++ frontend/src/reducers/product_constants.js
@@ -1,4 +1,10 @@
+// for fetching product name, price, and reviews
 export const ALL_PRODUCTS_FETCH = "ALL_PRODUCTS_FETCH";
 export const ALL_PRODUCTS_FAIL = "ALL_PRODUCTS_FAIL";
 export const ALL_PRODUCTS_SUCCESS = "ALL_PRODUCTS_SUCCESS";
 export const CLEAR_ERRORS = "CLEAR_ERRORS";
+
+// for fetching product details
+export const PRODUCT_DETAILS_FETCH = "PRODUCT_DETAILS_FETCH";
+export const PRODUCT_DETAILS_SUCCESS = "PRODUCT_DETAILS_SUCCESS";
+export const PRODUCT_DETAILS_FAIL = "PRODUCT_DETAILS_FAIL";
