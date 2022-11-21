195e9defb3f1d41c0deb065ae7c8f205cd2b43b2
asubedi <asubedi@go.olemiss.edu>
2022-11-21 17:24:29 -0600

    worked with signup reducer and actions

--- frontend/src/actions/user_actions.js
+++ frontend/src/actions/user_actions.js
@@ -2,6 +2,9 @@ import {
   LOGIN_FETCH,
   LOGIN_SUCCESS,
   LOGIN_FAIL,
+  REGISTER_FETCH,
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
   CLEAR_ERRORS,
 } from "../reducers/product_constants";
 import axios from "axios";
@@ -32,6 +35,35 @@ export const loginUser = (email, password) => async (dispatch) => {
   } catch (error) {
     dispatch({
       type: LOGIN_FAIL,
+      payload: error.response.data.message,
+    });
+  }
+};
+
+// signup
+
+export const signupUser = (userDetails) => async (dispatch) => {
+  try {
+    dispatch({
+      type: REGISTER_FETCH,
+    });
+
+    const config = {
+      header: {
+        "Content-Type": "multipart/form-data",
+      },
+    };
+
+    // fetch
+    const { data } = await axios.post("api/v1/register", userDetails, config);
+
+    dispatch({
+      type: REGISTER_SUCCESS,
+      payload: data.user,
+    });
+  } catch (error) {
+    dispatch({
+      type: REGISTER_FAIL,
       payload: error.response.data.message,
     });
   }
---
+++ frontend/src/components/Auth/Signup.js
@@ -0,0 +1,11 @@
+import React, { Fragment, useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router";
+import { loginUser, handleErrors } from "../../actions/user_actions";
+import "./Signup.css";
+
+const Signup = () => {
+  return <div></div>;
+};
+
+export default Signup;
