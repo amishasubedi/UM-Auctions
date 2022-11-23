a45bb3fb1cc59c6f5bad5ab2110055e3308796c0
asubedi <asubedi@go.olemiss.edu>
2022-11-22 21:05:36 -0600

    created dropdown menu for user

--- frontend/src/components/Layout/Header.js
+++ frontend/src/components/Layout/Header.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { Fragment } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import ProductSearch from "../BidItems/ProductSearch";
 import "./Header.css";
 
 const Header = () => {
+  const dispatch = useDispatch();
+
+  const { user, loading } = useSelector((state) => state.auth);
   return (
     <Fragment>
       <nav className="navbar row">
@@ -19,16 +23,61 @@ const Header = () => {
         </div>
 
         <div className="col-12 col-md-3 mt-4 mt-md-0 text-center">
-          <Link to="/login" className="btn" id="login_btn">
-            Login
+          <Link to="/cart" style={{ textDecoration: "none" }}>
+            <span id="cart" className="ml-3">
+              Cart
+            </span>
+            <span className="ml-1" id="cart_count">
+              2
+            </span>
           </Link>
 
-          <span id="cart" className="ml-3">
-            Cart
-          </span>
-          <span className="ml-1" id="cart_count">
-            2
-          </span>
+          {user ? (
+            <div className="ml-4 dropdown d-inline">
+              {" "}
+              <Link
+                to="#!"
+                className="btn dropdown-toggle text-white"
+                type="button"
+                id="dropDownMenuButton"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                <figure>
+                  <div>Welcome {user && user.name}</div>
+                </figure>
+              </Link>{" "}
+              {/* // create drop down menu for user */}
+              <div
+                className="dropdown-menu"
+                aria-labelledby="dropDownMenuButton"
+              >
+                {user && user.role !== "admin" ? (
+                  <Link className="dropdown-item" to="/orders/myOrder">
+                    MyBid
+                  </Link>
+                ) : (
+                  <Link className="dropdown-item" to="/dashboard">
+                    Dashboard
+                  </Link>
+                )}
+
+                <Link className="dropdown-item" to="/myprofile">
+                  Profile
+                </Link>
+                <Link className="dropdown-item text-danger" to="/">
+                  Logout
+                </Link>
+              </div>
+            </div>
+          ) : (
+            !loading && (
+              <Link to="/login" className="btn" id="login_btn">
+                Login
+              </Link>
+            )
+          )}
         </div>
       </nav>
     </Fragment>
