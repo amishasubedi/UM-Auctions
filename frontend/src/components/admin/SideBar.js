9508cbea471e11e1bc0913e117d6493f58422db8
asubedi <asubedi@go.olemiss.edu>
2022-11-27 21:55:40 -0600

    added sidebar ui

---
+++ frontend/src/components/admin/SideBar.js
@@ -0,0 +1,61 @@
+import React, { Fragment } from "react";
+
+import { Link } from "react-router-dom";
+
+const Sidebar = () => {
+  return (
+    <Fragment>
+      <div className="row">
+        <div className="col-2">
+          <div className="sidebar-wrapper">
+            <nav id="sidebar">
+              <ul className="list-unstyled components">
+                <li>
+                  <Link to="/dashboard">
+                    <i className="fas fa-tachometer-alt"></i> Dashboard
+                  </Link>
+                </li>
+
+                <li>
+                  <Link
+                    href="#productSubmenu"
+                    data-toggle="collapse"
+                    aria-expanded="false"
+                    className="dropdown-toggle"
+                  >
+                    <i className="fab fa-product-hunt"></i> Products
+                  </Link>
+                  <ul className="collapse list-unstyled" id="productSubmenu">
+                    <li>
+                      <Link to="/admin/products">
+                        <i className="fas fa-clipboard-list"></i> All
+                      </Link>
+                    </li>
+
+                    <li>
+                      <Link to="/admin/product">
+                        <i className="fas fa-plus"></i> Create
+                      </Link>
+                    </li>
+                  </ul>
+                </li>
+
+                <li>
+                  <Link href="/admin.orders">
+                    <i className="fas fa-shopping-basket"></i> Orders
+                  </Link>
+                </li>
+
+                <li>
+                  <Link href="/admin/users">
+                    <i className="fas fa-users"></i> Users
+                  </Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  );
+};
