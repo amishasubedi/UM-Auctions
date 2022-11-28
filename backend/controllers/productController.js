ea53172047f004290836b30a87af1950a52c2fcd
asubedi <asubedi@go.olemiss.edu>
2022-11-28 03:26:19 -0600

    delete product images

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -126,6 +126,12 @@ exports.deleteProduct = AsyncErrors(async (req, res, next) => {
     });
   }
 
+  for (let i = 0; i < product.images.length; i++) {
+    const result = await cloudinary.v2.uploader.destroy(
+      product.images[i].public_id
+    );
+  }
+
   await product.remove();
 
   res.status(200).json({
