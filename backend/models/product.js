d4865a63445e0c015d238b6ece1e096b969cd228
asubedi <asubedi@go.olemiss.edu>
2023-04-20 11:30:17 -0500

    untracked files updated

--- backend/models/product.js
+++ backend/models/product.js
@@ -64,12 +64,7 @@ const productSchema = new mongoose.Schema({
     type: String,
     required: [true, "Please enter product seller"],
   },
-  stock: {
-    type: Number,
-    required: [true, "Please enter product stock"],
-    maxLength: [5, "Product name cannot exceed 5 characters"],
-    default: 0,
-  },
+ 
   bidStart: {
     type: Date,
     default: defaultStartTime,
